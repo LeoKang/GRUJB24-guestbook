b12ca5f1854a92782e84702e554e488eb1c0aa0a
LeoKang <xeungjin@nate.com>
2024-07-16 15:47:49 +0900

    ~ 158 page

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -5,11 +5,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto) {
@@ -19,6 +22,8 @@ public class GuestbookServiceImpl implements GuestbookService {
         Guestbook entity = dtoToEntity(dto);
         log.info(entity);
 
-        return null;
+        repository.save(entity);
+
+        return entity.getGno();
     }
 }
